c7a099e35ae7fb9b1f4eb3f5a2328016613da7d9
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2018-09-14 00:38:43 -0500

    APLICACIÓN CON SYSTEM TRAY Y TRAY ICON PARA MANTENER LA APLICACIÓN EN SEGUNDO PLANO

---
+++ src/segundoplano/SegundoPlano.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundoplano;
+
+import vistas.index;
+
+/**
+ *
+ * @author Jesús Mendoza
+ */
+public class SegundoPlano {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        index ind = new index();
+        ind.setVisible(true);
+    }
+    
+}
